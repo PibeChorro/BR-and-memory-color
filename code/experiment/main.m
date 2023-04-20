985f2c1d23f855f09aa6db6c1078d77fc2f89433
Vincent Plikat <vincent.plikat@student.uni-tuebingen.de>
2023-04-20 16:21:26 +0200

    Added adjust fusion part
    TO-DO: make it a function. 
    Get Keyboard index in an elegant way

--- code/experiment/main.m
+++ code/experiment/main.m
@@ -79,6 +79,59 @@ try
     Screen ('Flip', ptb.window);
     WaitSecs (0.5);
     KbWait();
+
+    % fuse images to the eyes 
+    % TODO: make it a function
+    framesize = max([ptb.screenXpixels, ptb.screenYpixels])/3; 
+
+    while true
+%         Change parameters for side-by-side stereo display modes (4 and 5).
+%         SetStereoSideBySideParameters(win [, leftOffset][, leftScale][, rightOffset][, rightScale][, shaders][, offsetUnit='windowsizes'])
+        SetStereoSideBySideParameters(ptb.window, [0+horizontalOffset, 0+verticalOffset], [1, 1], [1-horizontalOffset, 0-verticalOffset], [1, 1]);
+
+        % Select   left-eye image buffer for drawing:
+        Screen('SelectStereoDrawBuffer', ptb.window, 0);
+        Screen('FrameRect', ptb.window, 0,  CenterRectOnPoint([0 0 framesize framesize], ptb.xCenter, ptb.yCenter), 10);
+
+        DrawFormattedText(ptb.window, 'Fusion test: use arrows to adjust or press "space" to continue',...
+        'center', 'center', 0, 20, [], [], [], [], CenterRectOnPoint([0 0 framesize framesize], ptb.xCenter, ptb.yCenter)); % is this needed?
+
+        % Select right-eye image buffer for drawing:
+        Screen('SelectStereoDrawBuffer', ptb.window, 1);
+        Screen('FrameRect', ptb.window, 0,  CenterRectOnPoint([0 0 framesize framesize], ptb.xCenter, ptb.yCenter), 10);
+    
+        DrawFormattedText(ptb.window, 'Fusion test: use arrows to adjust or press "space" to continue',...
+        'center', 'center', 0, 20, [], [], [], [], CenterRectOnPoint([0 0 framesize framesize], ptb.xCenter, ptb.yCenter)); % is this needed?
+
+        % Tell PTB drawing is finished for this frame:
+        Screen('DrawingFinished', ptb.window);
+        
+        Screen('Flip', ptb.window);
+
+        [KeyIsDown, ~, keyCode, ~] = KbCheck(7);
+
+        if KeyIsDown
+            if find(keyCode)==KbName('LeftArrow')
+                horizontalOffset = horizontalOffset-0.01;
+                disp('=>pressed LEFT')
+            elseif find(keyCode)==KbName('RightArrow')
+                horizontalOffset = horizontalOffset+0.01;
+                disp('=>pressed RIGHT')
+            elseif find(keyCode)==KbName('UpArrow')
+                verticalOffset = verticalOffset+0.01;
+                disp('=>pressed UP')
+            elseif find(keyCode)==KbName('DownArrow')
+                verticalOffset = verticalOffset-0.01;
+                disp('=>pressed Down')
+            elseif find(keyCode)==KbName('Space')
+                disp('=>pressed Space')
+                break
+            else
+                disp(['Pressed' num2str(KbName('LeftArrow'))])
+            end
+                fprintf('x-offset: %100.2f, y-offset:%100.2f \n', horizontalOffset, verticalOffset);
+        end
+    end
     
     
     % Loop over trials
