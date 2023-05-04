9bce9a3ce16b4bd0a7bcd9d050118d8e7bafdd77
Vincent Plikat <vincent.plikat@student.uni-tuebingen.de>
2023-05-04 16:28:37 +0200

    Updated instructions

--- code/experiment/designSettings.m
+++ code/experiment/designSettings.m
@@ -1,21 +1,25 @@
-function [design] = designSettings(language)
+function [design] = designSettings(language,design)
 
 if strcmp (language, 'german')
-    design.Introduction =       ['Vielen Dank dass du an unserer Studie teilnimmst\n\n'...
+    design.Introduction = ['Vielen Dank dass du an unserer Studie teilnimmst\n\n'...
         'Drücke eine beliebigen Knopf um fortzufahren'];
-    design.Instruction1 =       ['Drücke dies und drücke jenes'];
-    design.Instruction2 =       [''];
-    design.Instruction3 =       [''];
+    design.Instruction1 = ['Drücke dies und drücke jenes'];
+    design.Instruction2 = [''];
+    design.Instruction3 = [''];
+    
+    design.FusionText   = ['Fusionstest: nutze die Pfeiltasten, um das Feld einzustellen. \n'...
+        'Drücke "ENTER" um zu bestätigen'];
     
     design.RunIsOver =          '';
 else
     %% English version of instructions
-    design.Introduction =       ['Thank you very much for participating in our study.\n\n'...
+    design.Introduction = ['Thank you very much for participating in our study.\n\n'...
         'Press a botton to proceed.'];
-    design.Instruction1 =       ['Press this and press that'];
-    design.Instruction2 =       [''];
-    design.Instruction3 =       [''];
+    design.Instruction1 = ['Press this and press that'];
+    design.Instruction2 = [''];
+    design.Instruction3 = [''];
     
+    design.FusionText   = 'Fusion test: use arrows to adjust or press "ENTER" to continue';
     design.RunIsOver =          'This run is now over';
 end
 end
